39a2a29fefcf550c65e5d2d9bbc56b67b7946c8a
Thiago Monteiro <thiagomgonal@hotmail.com>
2025-09-09 00:59:48 -0300

    primeiro commit

--- BancoSemDados.sql
+++ BancoSemDados.sql
@@ -1,110 +1,97 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           8.0.41 - MySQL Community Server - GPL
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.10.0.7000
+-- Versão do servidor:           PostgreSQL 17.6 on x86_64-windows, compiled by msvc-19.44.35213, 64-bit
+-- OS do Servidor:               
+-- HeidiSQL Versão:              12.6.0.6765
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES  */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Copiando estrutura do banco de dados para simulado_db
-CREATE DATABASE IF NOT EXISTS `app_simulado` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `app_simulado`;
-
--- Copiando estrutura para tabela simulado_db.opcoes
-CREATE TABLE IF NOT EXISTS `opcoes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `questao_id` int NOT NULL,
-  `texto_opcao` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `correta` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `questao_id` (`questao_id`),
-  CONSTRAINT `opcoes_ibfk_1` FOREIGN KEY (`questao_id`) REFERENCES `questoes` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Copiando estrutura para tabela app_simulado.opcoes
+CREATE TABLE IF NOT EXISTS "opcoes" (
+	"id" INTEGER NOT NULL,
+	"questao_id" INTEGER NOT NULL,
+	"texto_opcao" TEXT NOT NULL,
+	"correta" BOOLEAN NOT NULL DEFAULT false,
+	PRIMARY KEY ("id"),
+	CONSTRAINT "opcoes_ibfk_1" FOREIGN KEY ("questao_id") REFERENCES "questoes" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela simulado_db.pontuacoes_por_tipo
-CREATE TABLE IF NOT EXISTS `pontuacoes_por_tipo` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `simulado_id` int NOT NULL,
-  `tipo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `corretas` int NOT NULL,
-  `total` int NOT NULL,
-  `porcentagem` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `simulado_id` (`simulado_id`),
-  CONSTRAINT `pontuacoes_por_tipo_ibfk_1` FOREIGN KEY (`simulado_id`) REFERENCES `simulados` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Copiando estrutura para tabela app_simulado.pontuacoes_por_tipo
+CREATE TABLE IF NOT EXISTS "pontuacoes_por_tipo" (
+	"id" INTEGER NOT NULL,
+	"simulado_id" INTEGER NOT NULL,
+	"tipo" VARCHAR(255) NOT NULL,
+	"corretas" INTEGER NOT NULL,
+	"total" INTEGER NOT NULL,
+	"porcentagem" NUMERIC(5,2) NOT NULL,
+	PRIMARY KEY ("id"),
+	CONSTRAINT "pontuacoes_por_tipo_ibfk_1" FOREIGN KEY ("simulado_id") REFERENCES "simulados" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela simulado_db.questoes
-CREATE TABLE IF NOT EXISTS `questoes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `autor_id` int NOT NULL,
-  `enunciado` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `tipo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `imagem_url` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `autor_id` (`autor_id`),
-  CONSTRAINT `questoes_ibfk_1` FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Copiando estrutura para tabela app_simulado.questoes
+CREATE TABLE IF NOT EXISTS "questoes" (
+	"id" INTEGER NOT NULL,
+	"autor_id" INTEGER NOT NULL,
+	"enunciado" TEXT NOT NULL,
+	"data_criacao" TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	"tipo" VARCHAR(50) NOT NULL,
+	"imagem_url" VARCHAR(250) NULL DEFAULT NULL,
+	PRIMARY KEY ("id"),
+	CONSTRAINT "questoes_ibfk_1" FOREIGN KEY ("autor_id") REFERENCES "usuarios" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela simulado_db.respostas_usuario
-CREATE TABLE IF NOT EXISTS `respostas_usuario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `simulado_id` int NOT NULL,
-  `questao_id` int NOT NULL,
-  `opcao_escolhida_id` int NOT NULL,
-  `data_resposta` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `simulado_id` (`simulado_id`),
-  KEY `questao_id` (`questao_id`),
-  KEY `opcao_escolhida_id` (`opcao_escolhida_id`),
-  CONSTRAINT `respostas_usuario_ibfk_1` FOREIGN KEY (`simulado_id`) REFERENCES `simulados` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `respostas_usuario_ibfk_2` FOREIGN KEY (`questao_id`) REFERENCES `questoes` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `respostas_usuario_ibfk_3` FOREIGN KEY (`opcao_escolhida_id`) REFERENCES `opcoes` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Copiando estrutura para tabela app_simulado.respostas_usuario
+CREATE TABLE IF NOT EXISTS "respostas_usuario" (
+	"id" INTEGER NOT NULL,
+	"simulado_id" INTEGER NOT NULL,
+	"questao_id" INTEGER NOT NULL,
+	"opcao_escolhida_id" INTEGER NOT NULL,
+	"data_resposta" TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ("id"),
+	CONSTRAINT "respostas_usuario_ibfk_1" FOREIGN KEY ("simulado_id") REFERENCES "simulados" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+	CONSTRAINT "respostas_usuario_ibfk_2" FOREIGN KEY ("questao_id") REFERENCES "questoes" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+	CONSTRAINT "respostas_usuario_ibfk_3" FOREIGN KEY ("opcao_escolhida_id") REFERENCES "opcoes" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela simulado_db.simulados
-CREATE TABLE IF NOT EXISTS `simulados` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `aluno_id` int NOT NULL,
-  `data_inicio` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `data_fim` timestamp NULL DEFAULT NULL,
-  `pontuacao` decimal(5,2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `aluno_id` (`aluno_id`),
-  CONSTRAINT `simulados_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Copiando estrutura para tabela app_simulado.simulados
+CREATE TABLE IF NOT EXISTS "simulados" (
+	"id" INTEGER NOT NULL,
+	"aluno_id" INTEGER NOT NULL,
+	"data_inicio" TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	"data_fim" TIMESTAMP NULL DEFAULT NULL,
+	"pontuacao" NUMERIC(5,2) NULL DEFAULT NULL,
+	PRIMARY KEY ("id"),
+	CONSTRAINT "simulados_ibfk_1" FOREIGN KEY ("aluno_id") REFERENCES "usuarios" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela simulado_db.usuarios
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `senha` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tipo_acesso` enum('aluno','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'aluno',
-  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Copiando estrutura para tabela app_simulado.usuarios
+CREATE TABLE IF NOT EXISTS "usuarios" (
+	"id" INTEGER NOT NULL,
+	"nome" VARCHAR(100) NOT NULL,
+	"email" VARCHAR(100) NOT NULL,
+	"senha" VARCHAR(255) NOT NULL,
+	"tipo_acesso" UNKNOWN NOT NULL DEFAULT 'aluno',
+	"data_criacao" TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ("id"),
+	UNIQUE INDEX "usuarios_email_key" ("email")
+);
 
 -- Exportação de dados foi desmarcado.
 
